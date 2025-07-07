3068c26553ab7fbd77d87586d9546a18c9892b90
dean171819 <deannasul4@gmail.com>
2025-07-07 13:55:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'HansTz&SsZ2gJIS#gge3SwJvXX-gRu42pZPdvvhvFl3MRJMAtchXwMSVCLU',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "HANS-XMD",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
+    OWNER_NAME: process.env.OWNER_NAME || "DEAN",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "256701692589",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'HANS-XMD',
+    BOT : process.env.BOT_NAME || 'DN',
     URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/mn835l.jpg',
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
